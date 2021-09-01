236727d0f84a56f9f6532f6aaa047d8cb6da5b2b
yamoo9 <yamoo9@naver.com>
2021-09-01 15:22:29 +0900

    [실습] 구조 분해 할당 활용 (배열, 객체)

--- src/chapter/06-destructing-assignment.test.js
+++ src/chapter/06-destructing-assignment.test.js
@@ -1,6 +1,6 @@
 /* 아래 ES5 코드를 ES6 코드로 변경합니다. ------------------------------------------------ */
 
-var state = Object.freeze({
+const state = Object.freeze({
   loading: false,
   error: null,
   data: [
@@ -11,13 +11,25 @@ var state = Object.freeze({
 });
 
 // 객체 속성과 일치하는 변수 선언 후, 속성 값 할당
-var loading = state.loading;
-var error = state.error;
-var data = state.data;
+const { loading, error, data } = state; // { loading, error, data }
+
+console.log(loading); // false
+console.log(error); // null
+console.log(data); // [ ... ]
+
+// var loading = state.loading;
+// var error = state.error;
+// var data = state.data;
 
 // 배열의 첫번째 아이템, 나머지 집합 추출
-var firstData = data[0];
-var restData = data.slice(1);
+// var firstData = data[0];
+// var restData = data.slice(1);
+
+// const [a, b, ...rest] = [10, 20, 30, 40];
+const [firstData, ...restData] = data; // [{}, {}, {}]
+
+console.log(firstData); // 10
+console.log(restData); // [20, 30, 40]
 
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
