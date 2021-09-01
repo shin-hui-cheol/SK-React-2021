00fbbda2a6478dee29c095623b1092b804af5934
yamoo9 <yamoo9@naver.com>
2021-09-01 14:06:28 +0900

    [실습] 화살표 함수, 템플릿 리터럴 표기법 (인터폴레이션)

--- src/chapter/01-arrow-function.test.js
+++ src/chapter/01-arrow-function.test.js
@@ -1,23 +1,31 @@
 /* ES6를 사용해 함수를 작성합니다. ------------------------------------------------------ */
 
-var fibonacci = function (n) {
-  // 함수 코드 로직을 작성합니다.
-  return n;
-};
+// 매개변수가 1개인 경우 괄호 생략 가능
+// 함수가 값을 바로 반환할 경우 블록, return 키워드 생략 가능
+const fibonacci = (n) =>
+  n <= 0 ? 0 : n === 1 ? 1 : fibonacci(n - 2) + fibonacci(n - 1);
 
-var numberWithComma = function (n) {
-  // 함수 코드 로직을 작성합니다.
-  return n.toString();
+const numberWithComma = (n) => {
+  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
 
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
 // fibonacci 유틸리티
+test('fibonacci 유틸리티', () => {
+  // 기대되는 값 (expected)
+  // 전달되는 값 (received)
+  expect(fibonacci(6)).toBe(8);
+  expect(fibonacci(12)).toBe(144);
+  expect(fibonacci(18)).toBe(2584);
+});
 // fibonacci(6)    →  8
 // fibonacci(12)   →  144
 // fibonacci(18)   →  2584
 
 // numberWithComma 유틸리티
-// numberWithComma(900);      → '900'
-// numberWithComma(9900);     → '9,900'
-// numberWithComma(1398700);  → '1,398,700'
+test('numberWithComma 유틸리티', () => {
+  expect(numberWithComma(900)).toBe('900');
+  expect(numberWithComma(9900)).toBe('9,900');
+  expect(numberWithComma(1398700)).toBe('1,398,700');
+});
--- src/chapter/02.1-template-literal.test.js
+++ src/chapter/02.1-template-literal.test.js
@@ -1,43 +1,47 @@
 /* 아래 ES5 코드를 ES6 코드로 변경합니다. ----------------------------------------------- */
 
-function renderCard(card) {
-  return (
-    '<div class="card">\
-      <img src="' + card.image.src + '" class="card-img-top" alt="' + card.image.alt  + '" />\
-      <div class="card-body">\
-        <h5 class="card-title">' + card.title + '</h5>\
-        <p class="card-text">' + card.description + '</p>\
-        <a href="' + card.link.href + '" class="btn btn-primary">' + card.link.text + '</a>\
-      </div>\
-    </div>\
-    '
-  );
-};
+const removeSpaceString = (string) => string.toString().replace(/\s/g, '');
 
-/* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
-// renderCard 렌더 유틸리티
-
-const rendered = renderCard({
-  title: 'React 펀드멘탈',
-  description: 'React 애플리케이션 및 라이브러리를 빌드하는 데 필요한 기본 개념을 배웁니다.',
-  image: {
-    src: 'react-fundamentals.webp',
-    alt: 'React Fundamentals',
-  },
-  link: {
-    href: '/react-fundamentals',
-    text: 'React fundamentals',
-  },
-});
-
-const expected = /* html */`
+const renderCard = (card) => /* html */ `
   <div class="card">
-    <img src="react-fundamentals.webp" class="card-img-top" alt="React Fundamentals" />
+    <img src="${card.image.src}" class="card-img-top" alt="${card.image.alt}" />
     <div class="card-body">
-      <h5 class="card-title">React 펀드멘탈</h5>
-      <p class="card-text">React 애플리케이션 및 라이브러리를 빌드하는 데 필요한 기본 개념을 배웁니다.</p>
-      <a href="/react-fundamentals" class="btn btn-primary">React fundamentals</a>      
+      <h5 class="card-title">${card.title}</h5>
+      <p class="card-text">${card.description}</p>
+      <a href="${card.link.href}" class="btn btn-primary">${card.link.text}</a>      
     </div>    
   </div>
 `;
 
+/* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
+// renderCard 렌더 유틸리티
+test('renderCard 렌더 유틸리티', () => {
+  const rendered = renderCard({
+    title: 'React 펀드멘탈',
+    description:
+      'React 애플리케이션 및 라이브러리를 빌드하는 데 필요한 기본 개념을 배웁니다.',
+    image: {
+      src: 'react-fundamentals.webp',
+      alt: 'React Fundamentals',
+    },
+    link: {
+      href: '/react-fundamentals',
+      text: 'React fundamentals',
+    },
+  });
+
+  const expected = `
+    <div class="card">
+      <img src="react-fundamentals.webp" class="card-img-top" alt="React Fundamentals" />
+      <div class="card-body">
+        <h5 class="card-title">React 펀드멘탈</h5>
+        <p class="card-text">React 애플리케이션 및 라이브러리를 빌드하는 데 필요한 기본 개념을 배웁니다.</p>
+        <a href="/react-fundamentals" class="btn btn-primary">React fundamentals</a>      
+      </div>    
+    </div>
+  `;
+
+  expect(removeSpaceString(rendered)).toHaveLength(
+    removeSpaceString(expected).length
+  );
+});
