44b9d589d56adf73848bb6eee1c73e0642158611
yamoo9 <yamoo9@naver.com>
2021-09-01 14:29:12 +0900

    [실습] 태그 템플릿 리터럴과 CSS in JS 스타일 방식

--- src/chapter/02.2-tagged-template-literals.test.js
+++ src/chapter/02.2-tagged-template-literals.test.js
@@ -1,7 +1,8 @@
-const node = {
+// fake dom node object
+const fakeNode = {
   name: 'figure',
   className: 'tagged-template-literals',
-  style: { cssText: '' },
+  style: { cssText: `margin:10px;color:salmon;` },
 };
 
 const styled = (styles, node) => {
@@ -13,6 +14,7 @@ const styled = (styles, node) => {
   return node;
 };
 
+// ES6
 const $styled = (node) => (styles) => {
   node.style.cssText = styles.reduce(
     (prevStyle, nextStyle) =>
@@ -22,8 +24,48 @@ const $styled = (node) => (styles) => {
   return node;
 };
 
+// ES5
+// HOF → HOC
+// eslint-disable-next-line no-unused-vars
+function curringStyled(node) {
+  return function innerFunction(styles /* [] */) {
+    node.style.cssText = styles.reduce(
+      (prevStyle, nextStyle) =>
+        (prevStyle + nextStyle.trim()).replace(/\s+/g, ''),
+      ''
+    );
+    return node;
+  };
+}
+
 /* 테스트 코드를 작성합니다. ----------------------------------------------------------- */
 
 // styled 유틸리티
+describe('styled 유틸리티 suite', () => {
+  beforeEach(() => {});
 
-// $styled 유틸리티
+  test('styled 유틸리티', () => {
+    const received = styled(['margin:10px;', 'color:salmon;'], fakeNode);
+
+    expect(received.style.cssText).toBe(fakeNode.style.cssText);
+  });
+
+  test('tagged template literals : styled 유틸리티', () => {
+    const received = styled`
+      ${fakeNode}
+      margin: 10px;
+      color: salmon;
+    `;
+
+    expect(received.style.cssText).toBe(fakeNode.style.cssText);
+  });
+
+  // $styled 유틸리티
+  test('$styled 유틸리티', () => {
+    const received = $styled(fakeNode)`
+      margin: 10px;
+      color: salmon;
+    `;
+    expect(received).toStrictEqual(fakeNode);
+  });
+});
