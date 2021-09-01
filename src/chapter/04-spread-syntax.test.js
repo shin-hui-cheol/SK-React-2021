0c8833fed1c25ad3a3dc072b0b2e688c811e8cbc
yamoo9 <yamoo9@naver.com>
2021-09-01 14:54:20 +0900

    [실습] 배열 전개

--- src/chapter/04-spread-syntax.test.js
+++ src/chapter/04-spread-syntax.test.js
@@ -4,11 +4,16 @@
 // 배열 전개
 // ----------------------------------------------------------------------------------
 
-var integers = [-1, 0, 32, -101, 24];
-var maxInt = Math.max.apply(Math, [20, -101].concat(integers));
+const integers = [-1, 0, 32, -101, 24];
+// Function.prototype.apply(thisArg, [arg1, arg2, ...])
+// var maxInt = Math.max.apply(Math, [20, -101].concat(integers));
+let maxInt = Math.max(20, -101, ...integers);
 
 // 테스트 코드를 작성합니다.
 // maxInt 결과 값
+test('maxInt 결과 값', () => {
+  expect(maxInt).toBe(32);
+});
 
 // ----------------------------------------------------------------------------------
 // 객체 전개
@@ -48,6 +53,8 @@ var setState = function (newState) {
 };
 
 // 상태 객체 (불변 데이터 화)
+// React = 선언형 프로그래밍 패러다임
+// 불변(immutable) 데이터 관리
 var state = Object.freeze({
   loading: false,
   error: null,
