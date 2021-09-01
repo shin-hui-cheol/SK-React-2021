cc703a184c69a8daab73fcf1e82dd33f8c7d4884
yamoo9 <yamoo9@naver.com>
2021-09-01 15:52:32 +0900

    [실습] ES 모듈 관리 (import, export, re-export)

--- src/chapter/utils/currency/currencyKR.js
+++ src/chapter/utils/currency/currencyKR.js
@@ -1,4 +1,8 @@
 // 1. numberWithComma 모듈을 불러옵니다.
+import numberWithComma from './numberWithComma';
 // 2. currencyKR 모듈을 "이름 내보내기"로 내보냅니다.
 
-const currencyKR = (n) => `${numberWithComma(n)}원`;
+export const currencyKR = (n) => `${numberWithComma(n)}원`;
+
+export const a = 'alpha';
+export const b = 'beta';
--- src/chapter/utils/index.js
+++ src/chapter/utils/index.js
@@ -1,2 +1,13 @@
+// import
+// named export
+// default export
+// re-export
+
 // 1. numberWithComma 모듈을 다시 내보냅니다.
+// 기본 내보내기 (export default)
+export { default as numberWithComma } from './currency/numberWithComma';
+
 // 2. currencyKR 모듈을 다시 내보냅니다.
+// 이름 내보내기 (export)
+// export { currencyKR, a, b } from './currency/currencyKR';
+export * from './currency/currencyKR';
